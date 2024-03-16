e99e98a7103f92d7be74aba579a8788756b4e045
soniankita12 <ankitasoni2211104@gmail.com>
2024-03-17 00:41:19 +0530

    done

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,6 +1,6 @@
 const Footer =()=>{
     return (<div className="bg-headingColor">
-        <h1 className="text-center  p-1 text-textColor">Made with ❤️ Ankita Soni</h1>
+        <h1 className="text-center  p-1 text-textColor">Made with ❤️ by Ankita Soni</h1>
     </div>);
 };
 
